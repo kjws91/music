996844c1802ef640d0f4e99b3b7aa16631503fb4
rheehoonkyung <crazyfou34@gmail.com>
2020-08-09 04:55:38 +0900

    배포시 jar -> war로 수정

--- src/main/java/com/music/music/MusicApplication.java
+++ src/main/java/com/music/music/MusicApplication.java
@@ -2,13 +2,20 @@ package com.music.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.music")
-public class MusicApplication {
+public class MusicApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MusicApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {	
+		return builder.sources(MusicApplication.class);
 	}
 
 }
